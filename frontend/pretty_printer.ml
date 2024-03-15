542279b1cac138280fe4009abc1cc3fb6bc2676a
Magnus K. Lundgren <magnus@themstrupvej.dk>
2024-03-15 11:19:38 +0100

    added simple pretty printer functionality

--- frontend/pretty_printer.ml
+++ frontend/pretty_printer.ml
@@ -1,3 +1,22 @@
 open Printf
+open Ast
+  
+  let rec pp_expr = function
+  | EConst n -> string_of_int n
+  | EIdent x -> x
+  | EBinop (op, e1, e2) -> 
+    let op_str = match op with
+    | Add -> "+"
+    | Sub -> "-"
+    | Mul -> "*"
+    | Div -> "/" in
+    "(" ^ pp_expr e1 ^ " " ^ op_str ^ " " ^ pp_expr e2 ^ ")"
+    
 
-let ppParse p = printf "hello world"
+let rec pp_stmt = function
+  | Slist exprs -> let stmt_list = List.map pp_stmt exprs in
+                  String.concat "\n" stmt_list
+  | Ssimple e -> pp_expr e
+
+let pp_prog prog =
+  pp_stmt prog.main
