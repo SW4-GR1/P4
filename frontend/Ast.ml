33ac26895f8d009288a36aab298a0f13ecdbbb8b
Magnus K. Lundgren <magnus@themstrupvej.dk>
2024-04-05 12:37:28 +0200

    partially added locations in parser

--- frontend/Ast.ml
+++ frontend/Ast.ml
@@ -1,3 +1,10 @@
+type loc = Lexing.position * Lexing.position
+
+type ident = { 
+  id : string; 
+  id_loc: loc 
+}
+
 type binop = Add | Mul | Sub | Div | Mod
 
 type unop = Inc | Dec 
@@ -18,10 +25,14 @@ type export =
 
 
 type expr =
+  { expr_node: expr_node;
+    expr_loc : loc }
+
+and expr_node =
   | EBool of bool
   | EConst of int
   | EFloat of float
-  | EIdent of string
+  | EIdent of ident
   | EBinop of binop * expr * expr
   | EUnop of string * unop
   | ECond of cond_binop * expr * expr
@@ -31,25 +42,31 @@ type expr =
   | Earray of expr list
 
 
+type stmt = 
+  { stmt_node: stmt_node;
+    stmt_loc : loc }
 
 (* type of statement *)
-type stmt =
+and stmt_node =
   | Ssimple of expr
   | Slist of stmt list
   | Sfunc of func
   | Sif of expr * stmt * stmt
   | Sreturn of expr
-  | Sdecl of type_ident * string * expr option 
-  | Sass of string * assign_type * expr 
-  | Sarr_decl of type_ident * expr * string * expr list option
+  | Sdecl of type_ident * ident * expr option 
+  | Sass of ident * assign_type * expr 
+  | Sarr_decl of type_ident * expr * ident * expr list option
   | Sarr_assign of string * assign_type * expr list 
   | Sarr_assign_elem of string * expr * assign_type * expr
-  | Sfor of stmt * expr * stmt * stmt
+  | Sfor of stmt_node * expr * stmt * stmt (*dec * cond * increment * body*)
   | Swhile of expr * stmt
+
+and arg_dec = type_ident * ident
+
 and func = {
   fun_type : type_ident;
   name : string;
-  args : (type_ident * string) list;
+  args : arg_dec list;
   body : stmt; 
 }
 
