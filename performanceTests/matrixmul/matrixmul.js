97a2daf56b74ba1b00025147d783a9763609bc1e
Lasse Drongesen <Lassemgp@gmail.com>
2024-04-10 11:00:23 +0200

    advancements matrixmul

---
+++ performanceTests/matrixmul/matrixmul.js
@@ -0,0 +1,22 @@
+function matrixMul(a, b) {
+    if (a[0].length !== b.length) {
+        throw new Error('Matrix dimensions do not match');
+    }
+
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result[i] = [];
+        for (let j = 0; j < b[0].length; j++) {
+            let sum = 0;
+            for (let k = 0; k < a[0].length; k++) {
+                sum += a[i][k] * b[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+}
+a = [[1, 2], [3, 4], [5, 6]];
+b = [[5, 6], [7, 8]];
+result = matrixMul(a, b); // [[19, 22], [43, 50]]
+console.log(result)
