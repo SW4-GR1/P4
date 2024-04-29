503bc9d2a9436228655dd80fec0697549e9ba779
Lasse <lassemgp@gmail.com>
2024-04-29 13:22:52 +0100

    Typechecker errors printing locations.

--- frontend/Typechecker.ml
+++ frontend/Typechecker.ml
@@ -29,18 +29,33 @@ let rec type_to_string = function
   | Tarr(t) -> "array of " ^ type_to_string t
 
 
+(* Function to convert location to string *)
+let string_of_location ((start_pos, end_pos) : loc) : string =
+  let line = start_pos.pos_lnum in
+  let start_char = start_pos.pos_cnum - start_pos.pos_bol in
+  let end_char = end_pos.pos_cnum - end_pos.pos_bol in
+  "line " ^ string_of_int line ^ 
+  ", start column " ^ string_of_int start_char ^ 
+  ", end column " ^ string_of_int end_char
+
+
 (* Exception for errors with optional location and message *)
-exception Error of Ast.loc option * string
+exception Error of string * string
 
 (* Function to raise an error with optional location and message *)
-let error ?loc msg = raise (Error (loc, msg))
+let error ?loc msg = 
+  let loc_str = match loc with
+    | Some location -> string_of_location location
+    | None -> "Unknown location"
+  in
+  raise (Error (loc_str, msg))
 
 (* Error functions for specific error types *)
-let unbound_variable x = error ("Unbound variable " ^ x)
-let unbound_function x = error ("Unbound function " ^ x)
-let duplicated_field x = error ("Duplicate declaration of: " ^ x)
-let incompatible_types t1 t2 = 
-  error ("Incompatible types " ^ type_to_string t1 ^ " and " ^ type_to_string t2)
+let unbound_variable ?loc x = error ?loc ("Unbound variable " ^ x)
+let unbound_function ?loc x = error ?loc ("Unbound function " ^ x)
+let duplicated_field ?loc x = error ?loc ("Duplicate declaration of: " ^ x)
+let incompatible_types ?loc t1 t2 = 
+  error ?loc ("Incompatible types " ^ type_to_string t1 ^ " and " ^ type_to_string t2)
 let bad_arity ?loc p a =
   error ?loc ("bad arity: function p expects " ^
               string_of_int a ^ " arguments")
@@ -50,14 +65,6 @@ let bad_arity ?loc p a =
 type funTable = (ty * ty list) symTab
 type varTable = ty symTab
 
-(* Function to convert location to string *)
-let string_of_location ((start_pos, end_pos) : loc) : string =
-  let line = start_pos.pos_lnum in
-  let start_char = start_pos.pos_cnum - start_pos.pos_bol in
-  let end_char = end_pos.pos_cnum - end_pos.pos_bol in
-  "line " ^ string_of_int line ^ 
-  ", start column " ^ string_of_int start_char ^ 
-  ", end column " ^ string_of_int end_char
 
   
   
@@ -82,12 +89,12 @@ let pp_funtype (args_res : Ttree.ty list * Ttree.ty) : string =
             ^ " -> " ^ type_to_string res
 
 (* Function to check for unique variables *)
-let check_unique (vl : (Ast.type_ident * Ast.ident) list) =
+(* let check_unique (vl : (Ast.type_ident * Ast.ident) list) =
   let set = Hashtbl.create 8 in 
   let check (_, {Ast.id = x}) =
     if Hashtbl.mem set x then duplicated_field x;
     Hashtbl.add set x () in
-  List.iter check vl
+  List.iter check vl *)
 
   (* Function to check if two types are equal *)
 let check_eq_type t1 t2 = match t1, t2 with
@@ -100,6 +107,7 @@ let check_eq_type t1 t2 = match t1, t2 with
 
 (* Function to check valid typing of operands in expressions *)  
 let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * expr =
+  let loc = exp.expr_loc in
   let expr_node = exp.expr_node in
   match expr_node with
   | EConst(c) -> ( Tint,  { expr_node = Econst(c); expr_ty = Tint } )
@@ -111,7 +119,7 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
       if is_some ident_type then
         let ty = get ident_type in 
         ( ty, { expr_node = Eident(id'); expr_ty = ty })
-      else error("Variable " ^ id' ^ " has not been declared")
+      else error ~loc ("Variable " ^ id' ^ " has not been declared")
 
   | EBinop(op, e1, e2) ->
         let (t1, e1_bin) = checkExp ftab vtab e1 in
@@ -119,8 +127,8 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         if is_number t1 then
           if check_eq_type t1 t2 
             then (t1, { expr_node = Ebinop(op, e1_bin, e2_bin); expr_ty = t1 } )
-          else incompatible_types t1 t2
-        else error("Binary operator should only be used on numbers")
+          else incompatible_types ~loc t1 t2
+        else error ~loc ("Binary operator should only be used on numbers")
   
   | ECond(op, e1, e2) -> let (t1, e1_bin) = checkExp ftab vtab e1 in
       let (t2, e2_bin) = checkExp ftab vtab e2 in
@@ -130,7 +138,7 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         | _ -> false in
       if check_eq_type t1 t2 && (is_number t1 || bool_allowed) then 
       (Tbool, { expr_node = Econd(op, e1_bin, e2_bin); expr_ty = Tbool } )
-      else incompatible_types t1 t2
+      else incompatible_types ~loc t1 t2
 
   | EUnop(e1, op) ->
       let (t1, e1_bin) = checkExp ftab vtab e1 in
@@ -139,7 +147,7 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
       else let str_of_op op = match op with 
         | Ast.Inc -> "++"
         | Ast.Dec -> "--" in
-          error (str_of_op op ^ " operator applied to a non-numeric type")
+          error ~loc (str_of_op op ^ " operator applied to a non-numeric type")
 
   | ELog(op, e1, e2) ->
       let (t1, e1_bin) = checkExp ftab vtab e1 in
@@ -149,13 +157,13 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         else let str_of_lop op = match op with
         | Ast.And -> "and" 
         | Ast.Or -> "or" in
-         error (str_of_lop op ^ " operator applied to a non-boolean type")
+         error ~loc (str_of_lop op ^ " operator applied to a non-boolean type")
          
 | ENot(e1) ->
       let (t1, e1_bin) = checkExp ftab vtab e1 in
       if t1 = Tbool
         then (Tbool, { expr_node = Enot(e1_bin); expr_ty = Tbool })
-        else error ("Not operator applied to a non-boolean type") 
+        else error ~loc ("Not operator applied to a non-boolean type") 
 | EArray(e_list) -> 
       let e_list' = List.map (fun e -> checkExp ftab vtab e) e_list in
       let e_types = List.map fst e_list' in
@@ -163,7 +171,7 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
       if all_same then
         let ty = List.hd e_types in
         (Tarr(ty), { expr_node = Earray(List.map snd e_list'); expr_ty = Tarr(ty) })
-      else error ("Array elements are not of the same type")
+      else error ~loc ("Array elements are not of the same type")
 
 | EArr_lookup(id, e1) ->
       let (e1_ty, e1') = checkExp ftab vtab e1 in
@@ -174,9 +182,9 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         | Tarr(ty) -> 
           if check_eq_type e1_ty Tint then
             (ty, { expr_node = Earr_lookup(id, e1'); expr_ty = ty })
-          else error("Index lookup must evaluate to an integer value")
-        | _ -> error ("Variable " ^ id ^ " is not an array."))
-      else error ("Variable " ^ id ^ " has not been declared.")
+          else  error ~loc ("Index lookup must evaluate to an integer value")
+        | _ ->  error ~loc ("Variable " ^ id ^ " is not an array."))
+      else  error ~loc ("Variable " ^ id ^ " has not been declared.")
 
   |EFcall(ident, args) -> 
     let id = ident.id in
@@ -188,9 +196,9 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         if List.fold_left2 (fun b (ty, _) fun_argty -> b && (check_eq_type ty fun_argty)) true args' arg_tys then
           let exprs = List.map(fun (ty, e) -> e) args' in
             (ret_ty, {expr_node = Efcall(id, exprs); expr_ty = ret_ty})
-        else error("Type of arguments in function call do not match")
-      else error("Expected " ^ string_of_int(List.length arg_tys) ^ " argument(s), but got " ^ string_of_int(List.length args'))
-    else error("Function " ^ id ^" has not been declared")
+        else error ~loc ("Type of arguments in function call do not match")
+      else error ~loc ("Expected " ^ string_of_int(List.length arg_tys) ^ " argument(s), but got " ^ string_of_int(List.length args'))
+    else error ~loc ("Function " ^ id ^" has not been declared")
 
   |EVector(e_list) -> 
     let e_list' = List.map (fun e -> checkExp ftab vtab e) e_list in
@@ -199,15 +207,16 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
     if all_same then
       let ty = List.hd e_types in
       (Tarr(ty), { expr_node = Evector(List.map snd e_list'); expr_ty = Tarr(ty) })
-    else error ("Vector elements are not of the same type") 
+    else error ~loc ("Vector elements are not of the same type") 
       
   |_ -> assert false
     
   let rec checkStmt (ftab : funTable) (vtab : varTable) (stmt : Ast.stmt) : funTable * varTable * stmt =
+    let loc = stmt.stmt_loc in
     let stmt_node = stmt.stmt_node in 
     match stmt_node with
     | Sreturn(e) ->
-        error("Return statement outside of function")
+         error ~loc ("Return statement outside of function")
     | Ssimple(e) -> 
       let (_t, e') = checkExp ftab vtab e in
        ( ftab, vtab, Ssimple(e') )
@@ -219,7 +228,7 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
       let (ftab'', vtab'', false_branch) = checkStmt ftab vtab fbranch in
       if is_bool cond_ty then
         (ftab, vtab, Sif(e', true_branch, false_branch))
-      else error("Condition must evaluate to a boolean value")
+      else error ~loc ("Condition must evaluate to a boolean value")
     
     | Sfor(dec, cond, inc, body) -> 
       let (ftab', vtab', dec') = checkStmt ftab vtab dec in
@@ -233,15 +242,15 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
       if is_bool cond_ty then
         if dec_ident = inc_ident then
           (ftab, vtab, Sfor(dec', cond', inc', body'))
-        else error("Increment must be performed on variable " ^ dec_ident)
-      else error("Condition must evaluate to a boolean value")
+        else error ~loc ("Increment must be performed on variable " ^ dec_ident)
+      else error ~loc ("Condition must evaluate to a boolean value")
 
     | Swhile (e, body) ->
       let (cond_ty, e') = checkExp ftab vtab e in
       let (ftab', vtab', body') = checkStmt ftab vtab body in
       if is_bool cond_ty then
         (ftab, vtab, Swhile(e', body'))
-      else error("Condition must evaluate to boolean value")
+      else error ~loc ("Condition must evaluate to boolean value")
 
     
     | Sfunc(fdec) -> 
@@ -266,13 +275,13 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
             if check_eq_type t ty' then 
               Some e'
             else
-              incompatible_types t ty'
+              incompatible_types ~loc t ty'
           | None -> None in 
 
         let vdec' = { var_ty = ty'; var_name = id'; var_expr = expr_option } in
         ( ftab, vtab', Sdecl(vdec') )
 
-      else duplicated_field id'
+      else duplicated_field ~loc id'
       
     | Sass(ident, ass_ty, e) -> 
       let id = ident in 
@@ -282,8 +291,8 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         let var_ty = get var_option in 
         if check_eq_type var_ty t then
           ( ftab, vtab, Sass(id, ass_ty, e') )
-        else incompatible_types var_ty t
-      else error ("Variable " ^ id ^ " has not been declared.")
+        else incompatible_types  ~loc var_ty t
+      else error ~loc ("Variable " ^ id ^ " has not been declared.")
       
     | Sarr_decl(ty, ident, size, e) ->
         let ty' = ty_of_pty ty in
@@ -299,12 +308,12 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
             if arg_types_correct then
               let expr_list = List.map (fun (elem_ty, expr) -> expr) checked_args in
               Some(expr_list)
-            else error ("An element in the array is not of the correct type")
+            else error ~loc ("An element in the array is not of the correct type")
           | None -> None in
           let adec' = {arr_ty = ty'; arr_name = name; arr_size = size'; arr_expr = expr_option } in
           ( ftab, vtab', Sarr_decl(adec') )
-        else duplicated_field name
-        else incompatible_types t_size Tint
+        else duplicated_field ~loc name
+        else incompatible_types ~loc t_size Tint
 
     | Sarr_assign(ident, assign_op, expr_list) -> 
       let array_exists = SymTab.lookup ident vtab in
@@ -312,29 +321,29 @@ let rec checkExp (ftab : funTable) (vtab : varTable) (exp : Ast.expr) : ty * exp
         let arr_ty = get array_exists in
         let ty' = match arr_ty with
           | Tarr(t) -> t 
-          | _ -> error(ident ^ " is not an array") in
+          | _ -> error ~loc (ident ^ " is not an array") in
         let checked_args =  List.map (fun elem -> checkExp ftab vtab elem) expr_list in 
         let arg_types_correct = List.fold_left(fun b (elem_ty, expr) -> b && (check_eq_type elem_ty ty')) true checked_args in
           if arg_types_correct then
             let expr_list' = List.map (fun (elem_ty, expr) -> expr) checked_args in
             (ftab, vtab, Sarr_assign(ident, assign_op, expr_list'))
-          else error("An element in the array is not of the correct type")
-      else error("Array "^ ident ^ " has not been declared")
+          else error ~loc ("An element in the array is not of the correct type")
+      else error ~loc ("Array "^ ident ^ " has not been declared")
     
     | Sarr_assign_elem(ident, index, aop, e) -> 
       let arr = SymTab.lookup ident vtab in
       if is_some arr then
         let arr_ty = match (get arr) with 
         | Tarr(ty) -> ty
-        | _ -> error("variable " ^ ident ^ " is not an array") in
+        | _ -> error ~loc ("variable " ^ ident ^ " is not an array") in
           let (index_ty, index') = checkExp ftab vtab index in
           let (ty', e') = checkExp ftab vtab e in
           if check_eq_type index_ty Tint then
             if check_eq_type arr_ty ty' then
               (ftab, vtab, Sarr_assign_elem(ident, index', aop, e'))
-            else error("The element you are trying to assign is not of the expected type: "^ (type_to_string arr_ty))
-          else error("Index lookup must evaluate to an integer value")
-      else error("Array " ^ ident ^ " has not been declared")
+            else error ~loc ("The element you are trying to assign is not of the expected type: "^ (type_to_string arr_ty))
+          else error ~loc ("Index lookup must evaluate to an integer value")
+      else error ~loc ("Array " ^ ident ^ " has not been declared")
       
 
     | Slist(stmts) -> 
@@ -360,36 +369,37 @@ and checkArgList (ftab : funTable) (vtab : varTable) (args : Ast.arg_dec list) :
       let vtab' = SymTab.bind arg_id arg_ty' vtab in 
       let (vtab_last, arglist') = checkArgList ftab vtab' arglist in
       (vtab_last, [(arg_ty', arg_id)] @ arglist')
-    else error("Function argument " ^ arg_ident.id ^ " has been declared elsewhere")
+    else let loc = arg_ident.id_loc in error ~loc ("Function argument " ^ arg_ident.id ^ " has been declared elsewhere")
 
 and checkFunBody (ftab : funTable) (vtab : varTable) (ftype : ty) (body : Ast.stmt) : stmt list = 
   let rec checkFunBody_aux (ftab : funTable) (vtab : varTable) (stmts : Ast.stmt list) =  
     match stmts with 
     | [] -> [] 
     | s::slist -> 
+      let loc = s.stmt_loc in
       match s.stmt_node with
       | Ast.Sreturn(e) -> 
         let (ty', e') = checkExp ftab vtab e in
         if check_eq_type ty' ftype then
           [Sreturn(e')]
-        else error("Return type does not match function declaration")
+        else error ~loc ("Return type does not match function declaration")
       | Ast.Sif(e, tbranch, fbranch) ->
         let (ty', e') = checkExp ftab vtab e in
         if not (check_eq_type ty' Tbool) then
-          error("Condition in if statement must be of type bool");
+          error ~loc ("Condition in if statement must be of type bool");
         let tbranch', fbranch' = 
           match tbranch.stmt_node, fbranch.stmt_node with
           | Slist t, Slist f -> 
             checkFunBody_aux ftab vtab t, checkFunBody_aux ftab vtab f
           | _ -> 
-            error("Expected Slist in branches of Sif")
+            error ~loc ("Expected Slist in branches of Sif")
       in
       [Sif(e', Slist(tbranch'), Slist(fbranch'))] @ checkFunBody_aux ftab vtab slist (* Vi ville miste alle asignments af variables her right?*)
       | _ ->
         let (ftab', vtab', s') = checkStmt ftab vtab s in
           [s'] @ checkFunBody_aux ftab' vtab' slist in
 
-  let body_list = match body.stmt_node with 
+  let body_list = let loc = body.stmt_loc in match body.stmt_node with 
   | Slist(s) -> 
     let has_return = List.exists (fun (stmt : Ast.stmt) -> 
       match stmt.stmt_node with
@@ -397,9 +407,9 @@ and checkFunBody (ftab : funTable) (vtab : varTable) (ftype : ty) (body : Ast.st
       | _ -> false
     ) s in
     if not has_return then
-      error("Function is missing a return statement");
+      error ~loc ("Function is missing a return statement");
     checkFunBody_aux ftab vtab s
-  | _ -> error("Function body must be a list of statements") in
+  | _ -> error ~loc ("Function body must be a list of statements") in
   body_list
   
   
