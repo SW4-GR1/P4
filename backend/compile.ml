1b73bdf96d8d00bd814cd363000072fcfa460e78
Lasse Drongesen <Lassemgp@gmail.com>
2024-05-03 15:12:36 +0200

    Co-authored-by: Magnus K. Lundgren <magnusklundgren@users.noreply.github.com> Co-authored-by: Nicklas Vistoft Kleemann <NicklasKleemann@users.noreply.github.com> Co-authored-by: Thomas1412 <Thomass1412@users.noreply.github.com>

--- backend/compile.ml
+++ backend/compile.ml
@@ -1,24 +1,21 @@
 open Wat
 open Frontend.Ttree
 
-let compile_const t c =
-match t with
-  | Tint -> S (Printf.sprintf "i32.const %d" c)
-  | Tlongint -> S (Printf.sprintf "i64.const %d" c)
-  | _ -> failwith "Unsupported type"
-
-  let compile_float t f =
-  match t with
-    | Tfloat -> S (Printf.sprintf "f32.const %f" f)
-    | Tlongfloat -> S (Printf.sprintf "f64.const %f" f)
-    | _ -> failwith "Unsupported type"
-
+    
+let compile_unop ty id op =
+  match (op : unop) with
+  | Inc -> let new_value = binop add ty (get_local id) (int_const ty 1)  in 
+    Cat ((set_local id new_value), get_local id)
+  | Dec -> let new_value = binop sub ty (get_local id) (int_const ty 1) in
+    Cat ((set_local id new_value), get_local id)
+  
+  
 let rec compile_stmt stmt = 
   match stmt with 
   | Ssimple e -> compile_expr e
   | Slist stmts -> compile_stmt_list stmts
+  | Sdecl vdec -> Nop
   | _ -> failwith "Unsupported statement"
-
 
 and compile_stmt_list stmts = 
   match stmts with
@@ -29,11 +26,11 @@ and compile_stmt_list stmts =
 and compile_expr e =
   let ty = e.expr_ty in
   match e.expr_node with
-    | Eident(id) -> S (Printf.sprintf "(get_local $%s)" id)
-    | Econst c -> compile_const ty c 
-    | Efloat f -> compile_float ty f
+    | Eident(id) -> get_local id
+    | Econst c -> int_const ty c 
+    | Efloat f -> float_const ty f
     | Ebinop (op, e1, e2) -> compile_binop op ty e1 e2 
-    (* | Eunop (id, op) ->  *)
+    | Eunop (id, op) -> compile_unop ty id op
     | _ -> failwith "Unsupported expression"
 
 and compile_binop op ty e1 e2 =
@@ -47,6 +44,21 @@ and compile_binop op ty e1 e2 =
   | Mod -> binop rem_s ty e1' e2'
   | _ -> failwith "Unsupported binary operator"
 
+and compile_logical_operators op e1 e2 comepile_expr = 
+  let e1' = compile_expr e1 in
+  let e2' = compile_expr e2 in
+  match (op : log_op) with 
+  | And -> 
+    Cat (e1', 
+      Cat (S "i32.const 1",
+        Cat (S "i32.and",
+          e2')))         
+  | Or ->
+    Cat (e1', 
+    Cat (S "i32.const 1",
+      Cat (S "i32.or",
+        e2')))
+  | _ -> failwith "Unsupported logical operator"
 
 
 
