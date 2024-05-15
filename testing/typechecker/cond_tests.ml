69a079927ab83c1ad9b230738d2dfa81ed85d865
Thomass1412 <thomas.lunoe@gmail.com>
2024-05-15 14:14:30 +0200

    revised

--- testing/typechecker/cond_tests.ml
+++ testing/typechecker/cond_tests.ml
@@ -10,14 +10,18 @@ let test_typechecker_cond_eq_integers test_ctxt =
   let ftab = mk_ftab in
   let vtab = mk_vtab in
   let (ty, expr) = Typechecker.checkExp ftab vtab ast in
-  assert_equal Ttree.Tbool ty  (* Expect the type of an equality expression to be boolean *)
+  let expected_out = "(BOOL((INT5)==(INT5)))" in
+  let ttree_string = remove_whitespace (Pp_type.pp_expr expr) in
+  assert_equal expected_out ttree_string 
 
 let test_typechecker_cond_neq_bools test_ctxt =
   let ast = mk_expr (ECond (Neq, mk_expr (EBool true), mk_expr (EBool false))) in
   let ftab = mk_ftab in
   let vtab = mk_vtab in
   let (ty, expr) = Typechecker.checkExp ftab vtab ast in
-  assert_equal Ttree.Tbool ty  (* Expect the type of an inequality expression to be boolean *)
+  let expected_out = "(BOOL((BOOLtrue)!=(BOOLfalse)))" in
+  let ttree_string = remove_whitespace (Pp_type.pp_expr expr) in
+  assert_equal expected_out ttree_string
 
 let test_typechecker_cond_incompatible_types test_ctxt =
   let ast = mk_expr (ECond (Eq, mk_expr (EConst 10), mk_expr (EBool true))) in
