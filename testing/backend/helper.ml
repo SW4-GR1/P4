4b43dca156a99a7e20041140516b027536c56781
Lasse <lassemgp@gmail.com>
2024-05-15 13:39:49 +0100

    backend fundec tests

--- testing/backend/helper.ml
+++ testing/backend/helper.ml
@@ -3,4 +3,6 @@ open Str
 
 let mk_expr e t  = {expr_node = e; expr_ty = t}
 
+let mk_fundec ty name args body = {fun_ty = ty; fun_name = name; fun_args = args; fun_body = body}
+
 let remove_whitespace s = Str.global_replace (Str.regexp "[ \t\n\r]+") "" s
