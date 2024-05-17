62cee612026869b07f41d08b37d5407413e51f2e
Thomass1412 <thomas.lunoe@gmail.com>
2024-05-17 13:16:59 +0200

    if done

--- testing/unittesting/typechecker/if_tests.ml
+++ testing/unittesting/typechecker/if_tests.ml
@@ -19,8 +19,19 @@ let test_typechecker_if_stmt_condition_is_boolean test_ctxt =
   let expected_out = "if((BOOLtrue)){(INT42)}else{(INT0)}" in
   let ttree_string = remove_whitespace (Pp_type.pp_stmt typechecked_ast) in
   assert_equal expected_out ttree_string
+let test_typechecker_if_stmt_condition_is_boolean_no_else test_ctxt = 
+  let ast = mk_stmt (Sif(mk_expr (EBool true), mk_stmt (Ssimple (mk_expr (EConst 42))), mk_stmt (Slist []))) in
+  let ftab = mk_ftab in 
+  let vtab = mk_vtab in
+  let (_, _, typechecked_ast) = Typechecker.checkStmt ftab vtab ast in
+  let expected_out = "if((BOOLtrue)){(INT42)}" in
+  let ttree_string = remove_whitespace (Pp_type.pp_stmt typechecked_ast) in
+  print_endline ("Expected: " ^ expected_out);
+  print_endline ("Actual: " ^ ttree_string);
+  assert_equal expected_out ttree_string
 
 let if_tests = "ifTests" >::: [
   "test_typechecker_if_stmt_condition_is_not_boolean" >:: test_typechecker_if_stmt_condition_is_not_boolean;
   "test_typechecker_if_stmt_condition_is_boolean" >:: test_typechecker_if_stmt_condition_is_boolean; 
+  "let test_typechecker_if_stmt_condition_is_boolean_no_else" >:: test_typechecker_if_stmt_condition_is_boolean_no_else
 ]
