2a8461a81a1276b9a7ab124d68845c47610567b5
Thomass1412 <thomas.lunoe@gmail.com>
2024-05-17 12:49:18 +0200

    fdec done

--- testing/unittesting/typechecker/fdec_tests.ml
+++ testing/unittesting/typechecker/fdec_tests.ml
@@ -4,7 +4,7 @@ open Frontend
 open Frontend.Ast
 open Frontend.Ttree
 
-let test_typechecker_func_no_args_no_body test_ctxt =
+let test_typechecker_func_two_args test_ctxt =
   let arg_dec = [(Int_ty, mk_ident "x"); (Bool_ty, mk_ident "y")]in 
   let fdec = {
     fun_type = Int_ty; 
@@ -18,11 +18,44 @@ let test_typechecker_func_no_args_no_body test_ctxt =
   let (_, _, typechecked_ast) = Typechecker.checkStmt ftab vtab ast in
   let expected_out = "INTf(INTx,BOOLy){(INT2)return(INT1)}" in
   let ttree_string = remove_whitespace (Pp_type.pp_stmt typechecked_ast) in
+  assert_equal expected_out ttree_string
+
+let test_typechecker_func_no_args test_ctxt =
+  let arg_dec = []in 
+  let fdec = {
+    fun_type = Int_ty; 
+    fun_name = mk_ident "f"; 
+    args = arg_dec; 
+    body = mk_stmt (Slist [ mk_stmt (Sreturn (mk_expr (EConst 1)))]);
+  } in
+  let ast = mk_stmt (Sfunc fdec) in
+  let ftab = mk_ftab in
+  let vtab = mk_vtab in
+  let (_, _, typechecked_ast) = Typechecker.checkStmt ftab vtab ast in
+  let expected_out = "INTf(){return(INT1)}" in
+  let ttree_string = remove_whitespace (Pp_type.pp_stmt typechecked_ast) in
   print_endline ("Expected: " ^ expected_out);
   print_endline ("Actual: " ^ ttree_string);
   assert_equal expected_out ttree_string
 
+let test_typechecker_func_no_args_no_return test_ctxt =
+  let arg_dec = []in 
+  let fdec = {
+    fun_type = Int_ty; 
+    fun_name = mk_ident "f"; 
+    args = arg_dec; 
+    body = mk_stmt (Slist []);
+  } in
+  let ast = mk_stmt (Sfunc fdec) in
+  let ftab = mk_ftab in
+  let vtab = mk_vtab in
+  let typecheck = fun () -> let _ = Typechecker.checkStmt ftab vtab ast in ()
+  in assert_raises (mk_error "Function is missing a return statement") typecheck
+
+  
 let fdec_tests = "fdecTests" >::: [
-  "test_typechecker_func_no_args_no_body" >:: test_typechecker_func_no_args_no_body;
+  "test_typechecker_func_two_args" >:: test_typechecker_func_two_args;
+  "test_typechecker_func_no_args" >:: test_typechecker_func_no_args;
+  "test_typechecker_func_no_args_no_return" >:: test_typechecker_func_no_args_no_return;
 
 ]
