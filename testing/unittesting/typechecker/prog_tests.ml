e40f1d4318f8746e495a5de1df1789cc641bfcc4
Lasse Drongesen <Lassemgp@gmail.com>
2024-05-20 12:49:09 +0200

    prog test added

--- testing/unittesting/typechecker/prog_tests.ml
+++ testing/unittesting/typechecker/prog_tests.ml
@@ -68,6 +68,82 @@ let test_typecheck_decl_of_var_in_fun_body_with_same_name_as_global_var ctxt =
   let typecheck = fun () -> let _ = Typechecker.program ast in () in
   assert_raises (mk_error "Duplicate declaration of: x") typecheck
 
+let test_typecheck_decl_of_var_in_fun_body_with_same_name_as_other_function ctxt =
+  let ast = mk_prog ([
+  ]) (Sglobal_list []) 
+    (Sfundec_list 
+    [ (mk_stmt 
+        (Sfunc 
+          (mk_fundec 
+            Int_ty 
+            "f" 
+            [] 
+            (Slist 
+              [ (mk_stmt (Sreturn (mk_expr (EConst 1))))]
+            )
+          )
+        )
+      );
+      (mk_stmt 
+        (Sfunc 
+          (mk_fundec 
+            Int_ty 
+            "g" 
+            [] 
+            (Slist 
+              [(mk_stmt (Sdecl { var_ty = Int_ty; var_name = mk_ident "f"; var_expr = None }))
+              ;(mk_stmt (Sreturn (mk_expr (EConst 1)))) 
+              ]
+            )
+          )
+        )
+      )
+
+    ]
+  ) in
+  let typecheck = fun () -> let _ = Typechecker.program ast in () in
+  assert_raises (mk_error "Duplicate declaration of: f") typecheck
+
+let test_typecheck_same_var_declared_in_2_functions ctxt =
+  let ast = mk_prog ([
+  ]) (Sglobal_list []) 
+    (Sfundec_list 
+    [ (mk_stmt 
+        (Sfunc 
+          (mk_fundec 
+            Int_ty 
+            "f" 
+            [] 
+            (Slist 
+              [ (mk_stmt (Sdecl { var_ty = Int_ty; var_name = mk_ident "x"; var_expr = None }))
+              ; (mk_stmt (Sreturn (mk_expr (EConst 1))))]
+            )
+          )
+        )
+      );
+      (mk_stmt 
+        (Sfunc 
+          (mk_fundec 
+            Int_ty 
+            "g" 
+            [] 
+            (Slist 
+              [(mk_stmt (Sdecl { var_ty = Int_ty; var_name = mk_ident "x"; var_expr = None }))
+              ;(mk_stmt (Sreturn (mk_expr (EConst 1)))) 
+              ]
+            )
+          )
+        )
+      )
+
+    ]
+  ) in
+  let typecheck = Typechecker.program ast in 
+  let expected_out = "INTf(){letINTxreturn(INT1)}INTg(){letINTxreturn(INT1)}" in
+  let ttree_string = remove_whitespace (Pp_type.pp_prog typecheck) in
+  assert_equal  ~printer:(fun x -> x) expected_out ttree_string
+
+
 
   let prog_tests = "Program typechecking tests" >::: [
     "Invalid export" >:: test_typecheck_invalid_export;
@@ -77,6 +153,8 @@ let test_typecheck_decl_of_var_in_fun_body_with_same_name_as_global_var ctxt =
     "Global var and function with same name" >:: test_typecheck_gvar_func_same_name;
     "Use of global var in function body" >:: test_typecheck_use_of_gvar_in_funcbody;
     "Decl of var in fun body with same name as global var" >:: test_typecheck_decl_of_var_in_fun_body_with_same_name_as_global_var;
+    "Decl of var in fun body with same name as other function" >:: test_typecheck_decl_of_var_in_fun_body_with_same_name_as_other_function;
+    "Same var declared in 2 functions" >:: test_typecheck_same_var_declared_in_2_functions;
   ]  
 
   
